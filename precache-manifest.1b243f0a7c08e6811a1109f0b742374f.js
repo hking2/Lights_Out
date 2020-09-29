799a9723116c98d47c13c23d0ff517e711848408
hking2 <hking6776@gmail.com>
2020-09-28 20:05:36 -0700

    Updates

---
+++ precache-manifest.1b243f0a7c08e6811a1109f0b742374f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d28938ec74b9811d701b3677ea32fd5",
+    "url": "/Lights_Out/index.html"
+  },
+  {
+    "revision": "89bd8cc77aa5e4f7b510",
+    "url": "/Lights_Out/static/css/main.14a44f10.chunk.css"
+  },
+  {
+    "revision": "bcf27ab4bc83ef827a98",
+    "url": "/Lights_Out/static/js/2.49265995.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/Lights_Out/static/js/2.49265995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89bd8cc77aa5e4f7b510",
+    "url": "/Lights_Out/static/js/main.600b82db.chunk.js"
+  },
+  {
+    "revision": "c7ec0611cbf4bd8431f7",
+    "url": "/Lights_Out/static/js/runtime-main.d2ea1a64.js"
+  }
+]);
